a5aa0a44192f07fb8de0da3cd3d0ddf5a32390c5
Tommy Zarnén <tommy@combinum.com>
2025-11-17 07:45:25 +0100

    added temp way to add rule

--- components/ui/layout/bottom-bar.tsx
+++ components/ui/layout/bottom-bar.tsx
@@ -1,24 +1,15 @@
 "use client";
 
-import {
-  FaAngleLeft,
-  FaAngleRight,
-  FaBars,
-  FaMagnifyingGlass,
-  FaPlus,
-} from "react-icons/fa6";
-import { Button } from "../button";
+import { CreateRecurrenceRuleDialog } from "@/components/recurrence-rules/create-recurrence-rule-dialog";
 import AppDialog from "@/components/shared/app-dialog";
-import { useState } from "react";
 import CreateTaskForm from "@/components/tasks/forms/create-task-form";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../dropdown-menu";
 import { useAuth } from "@/lib/hooks/use-auth";
+import { Repeat } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useState } from "react";
+import { FaAngleLeft, FaAngleRight, FaBars, FaMagnifyingGlass, FaPlus } from "react-icons/fa6";
+import { Button } from "../button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../dropdown-menu";
 
 const BottomBar = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -31,15 +22,17 @@ const BottomBar = () => {
       <FaAngleRight className="w-6 h-6 " />
       <FaMagnifyingGlass className="w-6 h-6 " />
 
+      <CreateRecurrenceRuleDialog>
+        <Repeat className="h-6 w-6" />
+      </CreateRecurrenceRuleDialog>
+
       <AppDialog
         isDialogOpen={isDialogOpen}
         setIsDialogOpen={setIsDialogOpen}
         dialogTrigger={<FaPlus className="w-6 h-6 " />}
         dialogTitle="Create Task"
         dialogDescription="Fill out the form to create a new task"
-        dialogContent={
-          <CreateTaskForm onAfterSubmit={() => setIsDialogOpen(false)} />
-        }
+        dialogContent={<CreateTaskForm onAfterSubmit={() => setIsDialogOpen(false)} />}
         dialogFooter={
           <Button onClick={() => setIsDialogOpen(false)} variant="destructive">
             Cancel
@@ -52,16 +45,10 @@ const BottomBar = () => {
         </DropdownMenuTrigger>
 
         <DropdownMenuContent>
-          <DropdownMenuItem
-            className="dark:hidden block"
-            onClick={() => setTheme("dark")}
-          >
+          <DropdownMenuItem className="dark:hidden block" onClick={() => setTheme("dark")}>
             Dark
           </DropdownMenuItem>
-          <DropdownMenuItem
-            className="dark:block hidden"
-            onClick={() => setTheme("light")}
-          >
+          <DropdownMenuItem className="dark:block hidden" onClick={() => setTheme("light")}>
             Light
           </DropdownMenuItem>
         </DropdownMenuContent>
